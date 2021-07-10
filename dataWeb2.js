bf5aee54e050d8f754db94f9bd7d8bbcb13ed222
Blas-U <79434789+Blas-U@users.noreply.github.com>
2021-07-09 23:28:27 -0500

    update

--- dataWeb2.js
+++ dataWeb2.js
@@ -7,7 +7,7 @@ $.ajax({
     w = document.getElementById("dataWeb");
     obj = JSON.parse(data)
     c = "Water: " + obj[0].aWater;
-    c + = "Eggs: " + obj[0].nEggs;
+    c += "Eggs: " + obj[0].nEggs;
     w.innerHTML = c
   }
 })

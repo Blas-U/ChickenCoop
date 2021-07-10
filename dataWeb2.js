dbe4fbbcac2675522f37f574d76e488df0ea63e1
Blas-U <79434789+Blas-U@users.noreply.github.com>
2021-07-09 23:53:46 -0500

    update

--- dataWeb2.js
+++ dataWeb2.js
@@ -9,6 +9,10 @@ $.ajax({
     n = obj.length
     c = "Water: " + obj[n-1].aWater + "<br>";
     c += "Eggs: " + obj[n-1].nEggs;
+    c += "Food: " + obj[n-1].aFood;
+    c += "Date: " + obj[n-1].date;
+    c += "Notes: " + obj[n-1].Notes;
+    c += "Name: " + obj[n-1].Name;
     w.innerHTML = c
   }
 })
